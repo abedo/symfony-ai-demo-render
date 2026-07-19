c46a01d4c2df255086ee88b4afda2d9724b26bf5
Wojciech Kaźmierczak <wojkaz@boozt.com>
2026-07-19 18:06:49 +0200

    Uruchamianie samego serwera Apache bez inicjalizacji i indeksowania bazy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 set -e
 
-# Inicjalizacja bazy wektorowej na Neon.tech i indeksowanie bloga przy starcie
-echo "==> Przygotowywanie bazy danych i wektorów..."
-php bin/console ai:store:setup ai.store.postgres.symfony_blog --force
-
-echo "==> Indeksowanie bloga Symfony..."
-php bin/console ai:store:index blog -vv
-
 echo "==> Uruchamianie serwera Apache..."
 exec apache2-foreground
